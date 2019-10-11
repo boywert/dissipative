4e971f36badc10fda7e2763d5b3d878e0a95d1bc
Chaichalit Srisawat <boywert@gmail.com>
2019-10-11 09:59:50 +0000

    quick commit

--- src/update_primitive_variables.c
+++ src/update_primitive_variables.c
@@ -140,7 +140,7 @@ void set_pressure_of_cell_internal(struct particle_data *localP, struct sph_part
 #ifdef DISSIPATIVE
   double alpha_D = 0.01;
   double dark_photon_mass = 1e-10;
-  printf("update pressure\n");
+  printf("update pressure %d P = %f\n",i, localSphP[i].Pressure);
   localSphP[i].Pressure = localSphP[i].Density * localSphP[i].EOSTemperature
     + 2.0 * M_PI * alpha_D * localSphP[i].Density
     * localSphP[i].Density / (dark_photon_mass*dark_photon_mass);
